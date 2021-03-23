6f5dfe4a83b99e1bb39630ad64dd15e227a6f804
Pieter van den Hombergh (homberghp) <p.vandenhombergh@fontys.nl>
2021-03-23 11:43:32 +0100

    formatting

--- GUI/src/main/java/fontys/frontend/CustomerController.java
+++ GUI/src/main/java/fontys/frontend/CustomerController.java
@@ -8,27 +8,28 @@ import javafx.scene.control.TextField;
 import businesslogic.BusinessLogicAPI;
 
 public class CustomerController {
-    
+
     @FXML
     TextField customerName, dateOfBirth;
-    
+
     private BusinessLogicAPI businessLogicAPI;
 
     public CustomerController() {
     }
-    
-    public CustomerController( BusinessLogicAPI logicAPI) {
+
+    public CustomerController( BusinessLogicAPI logicAPI ) {
         this.businessLogicAPI = logicAPI;
     }
 
     @FXML
     private void switchToSecondary() throws IOException {
-        GUIApp.setRoot("secondary");
+        GUIApp.setRoot( "secondary" );
     }
-    
+
     @FXML
-    private void storeCustomer(){
-        Customer c = businessLogicAPI.getCustomerManager().createCustomer(customerName.getText(), LocalDate.parse(dateOfBirth.getText()));
-        businessLogicAPI.getCustomerManager().add(c);
+    private void storeCustomer() {
+        Customer c = businessLogicAPI.getCustomerManager().createCustomer( 
+                customerName.getText(), LocalDate.parse( dateOfBirth.getText() ) );
+        businessLogicAPI.getCustomerManager().add( c );
     }
 }
